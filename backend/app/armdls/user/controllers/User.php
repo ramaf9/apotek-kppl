311826acf362365fdc70f37f354373b14586bae0
rama <yeyrama@gmail.com>
2016-09-27 19:41:15 +0700

    update documentation

--- backend/app/armdls/user/controllers/User.php
+++ backend/app/armdls/user/controllers/User.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require_once APPPATH . '/libraries/REST_Controller.php';
+// import required external library
+require_once APPPATH . '/libraries/REST_Controller.php'; // rest_api library
 
 class User extends REST_Controller{
 // Load model in constructor
@@ -10,47 +11,73 @@ public function __construct() {
 }
 // Server's Get Method
 public function data_get($id_param = NULL){
+	// retrieve data from get method
 	$id = $this->input->get('id');
+	// check if $id is null
 	if($id===NULL){
+		// set $id value as id from parameter
 		$id = $id_param;
 	}
+	// check if $id is still null
 	if ($id === NULL)
 	{
+		/*
+		 	call read method from user_model that will get
+			$id data from database
+
+		*/
 		$data = $this->User_model->read($id);
+		// check if $data return true
 		if ($data)
 		{
+			// send success response
 			$this->response($data, REST_Controller::HTTP_OK);
 		}
 		else
 		{
+			// send failed response
 			$this->response([
-			'status' => FALSE,
-			'error' => 'No users were found'
+				'status' => FALSE,
+				'error' => 'No users were found'
 			], REST_Controller::HTTP_NOT_FOUND);
 		}
 	}
+	/*
+		call read method from user_model that will get
+		$id user data from database
+
+	*/
 	$data = $this->User_model->read($id);
+	// check if $data return true
 	if ($data)
 	{
+		// send success response
 		$this->set_response($data, REST_Controller::HTTP_OK);
 	}
 	else
 	{
+		// send failed response
 		$this->set_response([
-		'status' => FALSE,
-		'error' => 'Record could not be found'
+			'status' => FALSE,
+			'error' => 'Record could not be found'
 		], REST_Controller::HTTP_NOT_FOUND);
 	}
 }
 // Server's Post Method
 public function data_post(){
+	// set $data array from post method
 	$data = array(
 		'u_username' => $this->input->post('username'),
-		'u_password' => $this->input->post('upassword'),
+		'u_password' => $this->input->post('password'),
 		'u_joindate' => date('y-m-d')
 	);
+	/*
+		call insert method from user_model that will get
+		insert new user to database
 
+	*/
 	$this->User_model->insert($data);
+	// send success response
 	$message = [
 		'status' => TRUE,
 		'message' => $data['u_username'].' created'
@@ -59,30 +86,61 @@ public function data_post(){
 }
 // Server's Put Method
 public function data_put(){
+	// retrieve data from stream
 	$data = $this->input->input_stream();
-	$this->User_model->update($data);
-	$message = [
-		'status' => TRUE,
-		'message' => $data['u_username'].' updated'
-	];
-	$this->set_response($message, REST_Controller::HTTP_CREATED);
+	/*
+		call update method from user_model that will update
+		$id user data from database
+
+	*/
+	$data = $this->User_model->update($data);
+
+	if ($data) {
+		// send success response
+		$message = [
+			'status' => TRUE,
+			'message' => 'update success'
+		];
+		$this->set_response($message, REST_Controller::HTTP_CREATED);
+	}
+	else{
+		// send success response
+		$message = [
+			'status' => FALSE,
+			'message' => 'update failed'
+		];
+		$this->set_response($message, REST_Controller::HTTP_OK);
+	}
+
+
 }
 // Server's Delete Method
 public function data_delete(){
+	// retrieve data from current third segment
 	$id = $this->uri->segment(3);
+	// check if $id is null
 	if($id===NULL){
+		// send failed response
 		$this->set_response([
 			'status' => FALSE,
 			'error' => 'ID cannot be empty'
 		], REST_Controller::HTTP_NOT_FOUND);
 	}
+	/*
+		call delete method from user_model that will remove
+		$id data from database
+
+	*/
 	$data = $this->User_model->delete($id);
+	// check if $data return true
 	if ($data)
 	{
+		// send success response
 		$this->set_response($data, REST_Controller::HTTP_OK);
 	}
 	else
 	{
+		// send failed response
 		$this->set_response([
 			'status' => FALSE,
 			'error' => 'Record could not be found'
@@ -90,27 +148,45 @@ public function data_delete(){
 	}
 }
 
+// Server's login method
 public function login_post(){
+	// retrieve login data from post method
 	$username = $this->input->post('username');
 	$password = $this->input->post('password');
+	// initialize empty array
 	$message = [];
-
-	if ($id===NULL || $pass===NULL ) {
-		# code...
-	}
-	$data = $this->User_model->check_password($username,$password);
-	if ($data) {
-		$message = [
-			'status' => TRUE,
-			'message' => 'Login success'
-		]
-	}
-	else{
+	// check if $username or $password is null
+	if ($username===NULL || $password===NULL ) {
+		// set success response
 		$message = [
 			'status' => FALSE,
 			'message' => 'Login failed'
 		]
 	}
+	else{
+		/*
+		 	call check_password method from user_model that will check
+			user's username and password from database
+
+		*/
+		$data = $this->User_model->check_password($username,$password);
+		// check if $data return true
+		if ($data) {
+			// set success response
+			$message = [
+				'status' => TRUE,
+				'message' => 'Login success'
+			]
+		}
+		else{
+			// set failed response
+			$message = [
+				'status' => FALSE,
+				'message' => 'Login failed'
+			]
+		}
+	}
+	// send response
 	$this->set_response($message, REST_Controller::HTTP_OK);
 
 }
--- backend/app/armdls/user/models/User_model.php
+++ backend/app/armdls/user/models/User_model.php
@@ -4,31 +4,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User_model extends CI_Model{
 // Read Query
 public function read($id){
+	// check if $id param is null
 	if($id===NULL){
 		$replace = "" ;
 	}
 	else{
 		$replace = "=$id";
 	}
-
+	// query get data $id data from database
 	$query = $this->db->query("select * from user where u_id".$replace);
+	// return $query as array
 	return $query->result_array();
 }
 // Insert/Create Query
 public function insert($data){
+	// query inserting new data to database
 	$this->db->insert('user', $data);
 	return TRUE;
 }
 // Delete Query
 public function delete($id){
+	// query delete $id data from database
 	$query = $this->db->query("delete from user where u_id=$id");
 	return TRUE;
 }
 // Update Query
 public function update($data){
+	// set $id from $data array
 	$id= $data['id'];
+	// query update $id data from database
 	$this->db->where('u_id',$id);
-	$this->db->update('user',$data);
+	$query = $this->db->update('user',$data);
+	// check if query return true
+	if ($query) {
+		return TRUE;
+	}
+	else{
+		return FALSE;
+	}
 }
 public function check_username($username){
 	if($username===NULL){
