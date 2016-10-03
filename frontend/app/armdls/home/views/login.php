dc47317eba1a4c1c7ffb4a3301c072db298bbe8e
Naufal Raihan N <naufalraihan.n@gmail.com>
2016-10-04 00:16:26 +0700

    coba pasang bootstrap

--- frontend/app/armdls/home/views/login.php
+++ frontend/app/armdls/home/views/login.php
@@ -1,50 +1,84 @@
-<?php include('header.php') ?>
-  <body class="login">
-    <div class="wrapper">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>E-Apotek | Login Page</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.min.css'); ?>">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/plugins/iCheck/square/green.css'); ?>">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>E</b>-Apotek</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Login</p>
+
+    <form action="../../index2.html" method="post">
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Username">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
       <div class="row">
-        <div class="col-lg-12">
-          <div class="brand text-center">
-            <h1>
-              <img src="assets/img/heart.png" width="75px" height="75px" margin-left="auto" margin-right= "auto">
-              E-Apotek
-            </h1>
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
           </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-12">
-          <form method="POST" action="home/login">
-            <fieldset class="text-center">
-       <?php
-       if(isset($message_error) && $message_error){
-          echo '<div class="alert alert-error">';
-            echo '<a class="close" data-dismiss="alert">×</a>';
-            echo '<strong>Oh snap!</strong> Change a few things up and try submitting again.';
-
-          echo '</div>';
-
-      }?>
-              <legend>Selamat Datang di E-Apotek</legend>
-              <div class="form-group">
-                <input class="form-control" placeholder="Username" type="text" name="username" autocomplete="off">
-              </div>
-              <div class="form-group">
-                <input class="form-control" placeholder="Password" type="password" name="password" autocomplete="off">
-              </div>
-              <!--<div class="text-center">
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox">
-                    Remember me on this computer
-                  </label>
-                </div>-->
-                <a><button class="btn btn-default" type="submit">Sign in</button></a>
-                <br>
-                <!--<a href="">Forgot password?</a>-->
-              </div>
-            </fieldset>
-          </form>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-success btn-block btn-flat">Sign In</button>
         </div>
+        <!-- /.col -->
       </div>
-    </div>
-<?php include('footer.php') ?>
+    </form>
+
+    <a href="#">I forgot my password</a><br>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 2.2.3 -->
+<script src="<?php echo base_url('assets/plugins/jQuery/jquery-2.2.3.min.js'); ?>"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url('assets/plugins/iCheck/icheck.min.js'); ?>"ss></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-green',
+      radioClass: 'iradio_square-green',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+</body>
+</html>
