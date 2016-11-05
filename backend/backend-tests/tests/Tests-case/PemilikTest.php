0a8431fe5cded28547b8a5830c8d17404b55d85d
rama <yeyrama@gmail.com>
2016-11-05 20:41:34 +0700

    unit test pemilik

---
+++ backend/backend-tests/tests/Tests-case/PemilikTest.php
@@ -0,0 +1,83 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Simple test class for showing how to test with Guzzle
+ */
+
+namespace Testcase;
+
+use Guzzle\Tests\GuzzleTestCase,
+    Guzzle\Plugin\Mock\MockPlugin,
+    Guzzle\Http\Message\Response,
+    Guzzle\Http\Client as HttpClient,
+    Guzzle\Service\Client as ServiceClient,
+    Guzzle\Http\EntityBody;
+
+class PemilikTest extends GuzzleTestCase
+{
+    protected $_client;
+    protected $url = 'http://localhost/APOTEK-KPPL/backend/user/';
+    protected $token;
+    protected $id = 'rama5';
+
+    public function setUp()
+    {
+        $this->_client = new ServiceClient();
+        $this->token = "";
+    }
+
+    public function testLoginRequests()
+    {
+        // The following request will get the mock response from the plugin in FIFO order
+        $data = [
+            'username' => $this->id,
+            'password' => $this->id
+        ];
+        $request = $this->_client->post($this->url.'login', array(), array('input'=>$data));
+        $request->getQuery()->set('view', 'recent_open_or_overdue');
+        $response = $request->send();
+        $body = $response->json();
+        // $header = $response->getHeaders();
+        // $body = $response->getBody();
+        // echo json_encode($response);
+        $this->token = 'Bearer '.$body['data']['token'];
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertTrue($body['status']);
+        $this->assertNotNull($body['data']['token']);
+        $data = [
+            'token' => $this->token
+        ];
+        return $data;
+    }
+
+    /**
+     * @depends testLoginRequests
+     */
+
+     public function testGetLaporanPenjualanList($data){
+         $request = $this->_client->get($this->url.'pemilik/laporan_ro?username='.$this->id);
+         $request->addHeader('authorization', $data['token']);
+         $response = $request->send();
+         $body = $response->json();
+         $this->assertEquals(200,$response->getStatusCode());
+         $this->assertNotNull($body);
+
+         return $data;
+     }
+     
+     /**
+      * @depends testLoginRequests
+      */
+
+     public function testGetLaporanPembelianList($data){
+         $request = $this->_client->get($this->url.'pemilik/laporan_po?username='.$this->id);
+         $request->addHeader('authorization', $data['token']);
+         $response = $request->send();
+         $body = $response->json();
+         $this->assertEquals(200,$response->getStatusCode());
+         $this->assertNotNull($body);
+
+         return $data;
+     }
+}
